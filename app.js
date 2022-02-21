8e91c935bdb1229e1e223aa02673c56cacdef903
markushung <markushung1127@gmail.com>
2022-02-21 21:45:31 +0800

    Adrian

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ const keyboard = document.querySelector('.key-container')
 const messageDisplay = document.querySelector('.message-container')
 
 
-const wordle = 'ILOVEU'
+const wordle = 'HAPPY'
 const keys = [
     'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
     'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L',
     'ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '⌫',
 ]
 const guessRows = [
-    ['','','','','',''],
-    ['','','','','',''],
-    ['','','','','',''],
-    ['','','','','',''],
-    ['','','','','',''],
-    ['','','','','',''],
+    ['','','','',''],
+    ['','','','',''],
+    ['','','','',''],
+    ['','','','',''],
+    ['','','','',''],
+    ['','','','',''],
 ]
 let currentRow = 0
 let currentTile = 0
@@ -88,7 +88,7 @@ const checkRow = () => {
         console.log('guess is ' + guess, 'wordle is ' + wordle)
         flipTile()
         if (wordle == guess) {
-            showMessage('Can I be your Valentine?')
+            showMessage('Happy Birthday Adrian?')
             isGameOver = true
             return
         } else {
